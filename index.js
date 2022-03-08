6bf0df7546f4e793043dabaee1a2a9efc5d29745
Kash15if <kash15if@yahoo.com>
2022-03-08 08:27:06 +0530

    tested verified token for auth api

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var jwt = require("jsonwebtoken");
 // var token = jwt.sign({ foo: "bar" }, "shhhhh");
 
 app.get("/", async function (req, res) {
-  var token = jwt.sign({ foo: "bar" }, "shhhhh");
+  var token = jwt.sign({ name: "Kashif_" }, "shhhhh");
 
   const salt = await bcrypt.genSalt();
 
@@ -20,6 +20,21 @@ app.get("/", async function (req, res) {
   console.log(passhash);
 
   res.send(token);
+});
+
+//authorization for api data by passing token in header
+app.get("/checkHeader", async function (req, res) {
+  console.log(req.headers);
+
+  const jwttoken = req.headers.authorization;
+  const TokenArray = jwttoken.split(" ");
+
+  console.log(TokenArray[1]);
+  const decoded = jwt.decode(TokenArray[1]);
+
+  console.log(decoded);
+  const person = decoded;
+  res.send(`username is ${person.name}`);
 });
 
 app.listen(3000);
