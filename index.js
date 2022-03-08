799a040ce91efb7f6ccedba5b3a8c9ea00d41b25
Kash15if <kash15if@yahoo.com>
2022-03-08 07:31:51 +0530

    added bcrypt dependency and test it

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 
+const bcrypt = require("bcrypt");
+
 var jwt = require("jsonwebtoken");
 // var token = jwt.sign({ foo: "bar" }, "shhhhh");
 
-app.get("/", function (req, res) {
+app.get("/", async function (req, res) {
   var token = jwt.sign({ foo: "bar" }, "shhhhh");
 
-  console.log(token);
+  const salt = await bcrypt.genSalt();
+
+  const passhash = await bcrypt.hash("Kashif", salt);
+
+  const comp = await bcrypt.compare("Kashif", passhash);
+
+  if (comp) console.log("Password Matched");
+
+  console.log(passhash);
 
   res.send(token);
 });
